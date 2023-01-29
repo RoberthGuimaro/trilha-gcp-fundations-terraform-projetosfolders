58d89abd9e431b284ac5f5368f5f6457621ea9a7
Roberth Guimaro <guimaroroberth@gmail.com>
2023-01-29 16:53:43 -0400

    Atualizaado Terraform, feito planejamento ORG

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "barbero-devops-iac"
+  project = "guimaro-devops-iac"
   region  = "us-central1"
   zone    = "us-central1-c"
   credentials = "${file("serviceaccount.yaml")}"
@@ -7,7 +7,7 @@ provider "google" {
 
 resource "google_folder" "Financeiro" {
   display_name = "Financeiro"
-  parent       = "organizations/540829645030"
+  parent       = "organizations/570228649030"
 }
 
 resource "google_folder" "SalesForce" {
@@ -26,19 +26,57 @@ resource "google_folder" "Producao" {
 }
 
 
-resource "google_project" "barberosa2-salesforce-dev" {
+resource "google_project" "guimaro-salesforce-dev" {
   name       = "SalesForce-Dev"
-  project_id = "barberosa2-salesforce-dev"
+  project_id = "guimaro-salesforce-dev"
   folder_id  = google_folder.Desenvolvimento.name
   auto_create_network=false
-  billing_account = "018973-A8340F-83D8E5"
+  billing_account = "198945-C8270P-33F8R9"
 
 }
 
-# resource "google_project" "barberosa-salesforce-prod" {
-#   name       = "SalesForce-Prod"
-#   project_id = "barberosa-salesforce-prod"
-#   folder_id  = google_folder.Producao.name
-#   auto_create_network=false
-#   billing_account = "01B2C4-70459E-F53446"
-# }
+resource "google_project" "guimaro-salesforce-prod" {
+  name       = "SalesForce-Prod"
+  project_id = "guimaro-salesforce-prod"
+  folder_id  = google_folder.Producao.name
+  auto_create_network=false
+  billing_account = "198945-C8270P-33F8R9"
+ }
+
+ resource "google_folder" "Comercial" {
+  display_name = "Comercial"
+  parent       = "organizations/570228649030"
+}
+
+resource "google_folder" "Mobile" {
+  display_name = "Mobile"
+  parent       = google_folder.comercial.name
+}
+
+resource "google_folder" "Desenvolvimento" {
+  display_name = "Desenvolvimento"
+  parent       = google_folder.Mobile.name
+}
+
+resource "google_folder" "Producao" {
+  display_name = "Producao"
+  parent       = google_folder.Mobile.name
+}
+
+
+resource "google_project" "guimaro-comercial-mobile-dev" {
+  name       = "Comercial-Mobile-Dev"
+  project_id = "guimaro-comercial-mobile-dev"
+  folder_id  = google_folder.Desenvolvimento.name
+  auto_create_network=false
+  billing_account = "198945-C8270P-33F8R9"
+
+}
+
+resource "google_project" "guimaro-comercial-mobile-prod" {
+  name       = "Comercial-Mobile-Prod"
+  project_id = "guimaro-comercial-mobile-prod"
+  folder_id  = google_folder.Producao.name
+  auto_create_network=false
+  billing_account = "198945-C8270P-33F8R9"
+ }
